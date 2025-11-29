45398a86886c93f7133756ba3dfb9c6a3761a717
Brandon <tricksterguy87@gmail.com>
2025-11-28 23:54:34 -0800

    BattleTriggeredEventsPatch, this prevents error logging whenever custom triggers fire.
    And also enables Transcendimp-like effects that work off custom triggers.

---
+++ TrainworksReloaded.Plugin/Patches/BattleTriggeredEventsPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using TrainworksReloaded.Core;
+using TrainworksReloaded.Core.Interfaces;
+using static BattleTriggeredEvents;
+
+namespace TrainworksReloaded.Plugin.Patches
+{
+    [HarmonyPatch(typeof(BattleTriggeredEvents), nameof(BattleTriggeredEvents.Init))]
+    public class CustomBattleTriggeredEventsPatch
+    {
+        private static IRegister<CharacterTriggerData.Trigger>? triggerRegister;
+
+        public static void Postfix(Dictionary<Team.Type, TriggerDictionaries> ___teamTriggerDictionaries)
+        {
+            // Guaranteed to be called after Trainworks initializes.
+            triggerRegister ??= Railend.GetContainer().GetInstance<IRegister<CharacterTriggerData.Trigger>>();
+            Array team_types = Enum.GetValues(typeof(Team.Type));
+
+            foreach (Team.Type item in team_types)
+            {
+                var triggerDictionaries = ___teamTriggerDictionaries[item];
+                foreach (var trigger in triggerRegister.Values)
+                {
+                    if (triggerDictionaries.triggerDictionary.ContainsKey(trigger))
+                        continue;
+                    triggerDictionaries.triggerDictionary[trigger] = new BattleTriggers();
+                }
+            }
+        }
+    }
+}
