0d27b27b8f929f47ef2c5cd2c932f64e75015fab
Brandon <tricksterguy87@gmail.com>
2026-08-27 15:09:26 -0700

    1. Allow for the NonAdditive Shader to be used, if set it will be assigned later as this shader is not loaded at creation time 2. Simplify SpriteBased card art. 3. Allow for Animation clips to be added to both types of game objects.

--- TrainworksReloaded.Base/Prefab/GameObjectCardArtDecorator.cs
+++ TrainworksReloaded.Base/Prefab/GameObjectCardArtDecorator.cs
@@ -6,8 +6,11 @@ using System.Linq;
 using System.Reflection;
 using TrainworksReloaded.Base.Extensions;
 using TrainworksReloaded.Core.Extensions;
+using TrainworksReloaded.Core.Impl;
 using TrainworksReloaded.Core.Interfaces;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 using static TrainworksReloaded.Base.Extensions.ParseReferenceExtensions;
 
@@ -19,25 +22,36 @@ namespace TrainworksReloaded.Base.Prefab
         private readonly IRegister<Texture2D> textureRegister;
         private readonly IRegister<Sprite> spriteRegister;
         private readonly IModLogger<GameObjectCardArtDecorator> logger;
+        private readonly IRegister<AssetBundle> assetBundleRegister;
+        private readonly PluginAtlas atlas;
+        private readonly GameDataClient gameDataClient;
 
         private static readonly Lazy<Material> DefaultCardArtMaterial = new(() => Resources.FindObjectsOfTypeAll<Material>().FirstOrDefault(m => m.name == "CardMaterial_PunkrockReveler"));
         private static readonly FieldInfo LayerTransformsField = AccessTools.Field(typeof(AnimateCardEffects), "_layerTransforms");
+        private static readonly FieldInfo FieldsTurnedOnField = AccessTools.Field(typeof(AnimateCardEffects), "_fieldsTurnedOn");
         private static readonly FieldInfo TransformPositionField = AccessTools.Field(typeof(CardEffectTransform), "position");
         private static readonly FieldInfo TransformRotationField = AccessTools.Field(typeof(CardEffectTransform), "rotation");
         private static readonly FieldInfo TransformScaleField = AccessTools.Field(typeof(CardEffectTransform), "scale");
+        private static readonly List<Image> RequiresNonAdditiveShader = [];
+        private GameObject? CardArt_Spell_Begone;
 
         public GameObjectCardArtDecorator(
             IDataPipeline<IRegister<GameObject>, GameObject> decoratee,
             IModLogger<GameObjectCardArtDecorator> logger,
             IRegister<Texture2D> textureRegister,
-            IRegister<Sprite> spriteRegister
+            IRegister<Sprite> spriteRegister,
+            IRegister<AssetBundle> assetBundleRegister,
+            GameDataClient gameDataClient,
+            PluginAtlas atlas
         )
         {
             this.decoratee = decoratee;
             this.logger = logger;
             this.textureRegister = textureRegister;
             this.spriteRegister = spriteRegister;
-            
+            this.assetBundleRegister = assetBundleRegister;
+            this.atlas = atlas;
+            this.gameDataClient = gameDataClient;
         }
 
         public List<IDefinition<GameObject>> Run(IRegister<GameObject> service)
@@ -46,6 +60,47 @@ namespace TrainworksReloaded.Base.Prefab
             foreach (var definition in definitions)
             {
                 Setup(definition);
+            }
+            if (RequiresNonAdditiveShader.Count > 0)
+            {
+                if (gameDataClient.TryGetProvider<SaveManager>(out var saveManager))
+                {
+                    var card = saveManager.GetAllGameData().FindCardDataByName("Begone");
+                    List<AssetReference> assets = [];
+                    card?.GetAddressableAssets(assets);
+                    if (!assets.IsNullOrEmpty())
+                    {
+                        var handle = assets[0].LoadAsset<GameObject>();
+                        handle.Completed += op =>
+                        {
+                            if (op.Status != AsyncOperationStatus.Succeeded)
+                            {
+                                logger.Log(LogLevel.Error, "Failed to load addressable: " + assets[0].RuntimeKey.ToString());
+                                return;
+                            }
+
+                            CardArt_Spell_Begone = op.Result;
+                            var shader = CardArt_Spell_Begone?.transform.Find("CardSprite").GetComponent<Image>().material.shader;
+                            if (shader == null)
+                            {
+                                logger.Log(LogLevel.Error, "Non-Additive shader not found, can not update materials to use the shader.");
+                                return;
+                            }
+                            foreach (var image in RequiresNonAdditiveShader)
+                            {
+                                var oldMaterial = image.material;
+                                image.material = new Material(shader);
+                                image.material.CopyPropertiesFromMaterial(oldMaterial);
+
+                            }
+                            logger.Log(LogLevel.Info, $"Successfully updated {RequiresNonAdditiveShader.Count} material's shaders");
+                        };
+                    }
+                    else
+                    {
+                        logger.Log(LogLevel.Error, "Could not find Begone's Addressable Assets");
+                    }
+                }
             }
             return definitions;
         }
@@ -58,14 +113,17 @@ namespace TrainworksReloaded.Base.Prefab
 
             var config = definition.Configuration.GetSection("extensions").GetSection("card_art");
             if (config.GetSection("base_layer").Exists())
-                SetupAnimatedCardArt(definition, config);
+                SetupLayeredCardArt(definition, config);
             else
-                SetupStaticCardArt(definition, config);
+                SetupSpriteBasedCardArt(definition, config);
+
+            SetupAnimation(definition, config, definition.Data);
         }
 
-        private void SetupAnimatedCardArt(IDefinition<GameObject> definition, IConfiguration config)
+        private void SetupLayeredCardArt(IDefinition<GameObject> definition, IConfiguration config)
         {
-            EffectLayer[] layers = new EffectLayer[9];            
+            EffectLayer[] layers = new EffectLayer[9];
+            int[] fieldsTurnedOn = new int[8];
             EffectLayer base_layer = ParseEffectLayer(definition.Key, config.GetSection("base_layer"));
             if (base_layer.texture == null)
             {
@@ -79,6 +137,7 @@ namespace TrainworksReloaded.Base.Prefab
             {
                 var effectLayer = ParseEffectLayer(definition.Key, layerConfig);
                 layers[index] = effectLayer;
+                fieldsTurnedOn[index - 1] = effectLayer.animated ? 1 : 0;
                 index++;
             }
 
@@ -94,7 +153,7 @@ namespace TrainworksReloaded.Base.Prefab
             gameObject.AddComponent<AddressableAssetPrefab>();
             gameObject.AddComponent<RectTransform>();
 
-            var cardArt = new GameObject { name = "CardSprite" };
+            var cardArt = new GameObject { name = "CardSprite", layer = 5 };
             cardArt.transform.SetParent(gameObject.transform);
             var canvasRenderer = cardArt.AddComponent<CanvasRenderer>();
 
@@ -114,12 +173,24 @@ namespace TrainworksReloaded.Base.Prefab
 
             var animateCardEffects = cardArt.AddComponent<AnimateCardEffects>();
             CardEffectTransform[]? cardEffectTransforms = LayerTransformsField.GetValue(animateCardEffects) as CardEffectTransform[];
+            FieldsTurnedOnField.SetValue(animateCardEffects, fieldsTurnedOn);
             var shader = config.GetSection("shader").Value ?? "Shiny Shoe/CardEffects";
+            bool requiresNonAdditive = false;
+            if (shader == "Shiny Shoe/CardEffects Non-Additive")
+            {
+                // The shader isn't loaded at creation time so create with the normal shader and deferred reassignment til later.
+                requiresNonAdditive = true;
+                shader = "Shiny Shoe/CardEffects";
+            }
 
             var material = new Material(Shader.Find(shader))
             {
                 name = $"CardMaterial_{gameObject.name}"
             };
+            if (requiresNonAdditive)
+            {
+                RequiresNonAdditiveShader.Add(image);
+            }
             material.CopyPropertiesFromMaterial(DefaultCardArtMaterial.Value);
 
             for (int i = 0; i < layers.Length; i++)
@@ -146,6 +217,104 @@ namespace TrainworksReloaded.Base.Prefab
                 foreach (var transformIndex in effectTransform.layers)
                     cardEffectTransforms![transformIndex] = transformComponent;
             }
+        }
+
+        private void SetupAnimation(IDefinition<GameObject> definition, IConfiguration config, GameObject gameObject)
+        {
+            var cardArt = gameObject.transform.Find("CardSprite").gameObject;
+            var image = cardArt.GetComponent<Image>();
+
+            (AnimationClip? animationClip, List<Sprite>? sprites) = ParseAnimationClip(config.GetSection("animation_clip"), definition.Key);
+            if (animationClip != null)
+            {
+                Animation animation = gameObject.GetComponent<Animation>() ?? gameObject.AddComponent<Animation>();
+                if (sprites != null)
+                {
+                    var driver = cardArt.AddComponent<CardSpriteFrameDriver>();
+                    var transform = cardArt.AddComponent<CardEffectTransform>();
+                    driver.Frames = [.. sprites];
+                    driver.TargetImage = image;
+                    driver.FrameHolder = transform;
+                }
+
+                animation.AddClip(animationClip, animationClip.name);
+                var state = animation[animationClip.name];
+                if (state != null)
+                {
+                    state.wrapMode = WrapMode.Loop;
+                    state.enabled = true;
+                    state.weight = 1f;
+                }
+                animation.clip = animationClip;
+                animation.wrapMode = animationClip.wrapMode;
+
+            }
+        }
+
+        private (AnimationClip?, List<Sprite>?) ParseAnimationClip(IConfigurationSection configuration, string key)
+        {
+            if (!configuration.Exists()) 
+                return (null, null);
+
+            AnimationClip animation = new()
+            {
+                name = configuration.GetSection("id").ParseString() ?? "",
+                frameRate = configuration.GetSection("sample_rate").ParseFloat() ?? 60f,
+                wrapMode = configuration.GetSection("loop_time").ParseString() == "loop" ? WrapMode.Loop : WrapMode.Once,
+                legacy = true
+            };
+            
+
+            Assembly baseGame = typeof(CardEffectSacrifice).Assembly;
+
+            foreach (var config in configuration.GetSection("curves").GetChildren())
+            {
+                string path = config.GetSection("path").ParseString() ?? "";
+                string typeStr = config.GetSection("type").ParseString() ?? "";
+                Type type = baseGame.GetType(typeStr);
+                string attribute = config.GetSection("attribute").ParseString() ?? "";
+                var curve = new AnimationCurve();
+                foreach (var kfconfig in config.GetSection("keyframes").GetChildren())
+                {
+                    var time = kfconfig.GetSection("time").ParseFloat() ?? 0f;
+                    var value = kfconfig.GetSection("value").ParseFloat() ?? 0f;
+                    var in_tangent = kfconfig.GetSection("in_tangent").ParseFloat() ?? 0;
+                    var out_tangent = kfconfig.GetSection("out_tangent").ParseFloat() ?? 0;
+                    Keyframe kf = new(time, value, in_tangent, out_tangent);
+                    curve.AddKey(kf);
+                }
+                animation.SetCurve(path, type, attribute, curve);
+            }
+
+            var spriteReferences = configuration.GetSection("frames").ParseReferences();
+            List<Sprite>? sprites = [];
+            foreach (var reference in spriteReferences)
+            {
+                if (spriteRegister.TryLookupName(reference!.ToId(key, TemplateConstants.Sprite), out var sprite, out var _, reference.context))
+                {
+                    sprites.Add(sprite);
+                }
+            }
+
+            float frameDuration = 1f / animation.frameRate;
+            if (!sprites.IsNullOrEmpty())
+            {
+                var curve = new AnimationCurve();
+                for (int i = 0; i < sprites.Count; i++)
+                {
+                    curve.AddKey(new Keyframe(i * frameDuration, (float)i, 0f, float.PositiveInfinity));
+                }
+                curve.AddKey(new Keyframe(sprites.Count * frameDuration, (float)(sprites.Count - 1), 0f, 0f));
+                curve.preWrapMode = WrapMode.Loop;
+                curve.postWrapMode = WrapMode.Loop;
+                /// Sigh this is such a stupid hack, Can't use a custom class here because Unity Monobehaviours have to be registered
+                /// No way to register a new Script class to use here from a mod so we use a class with a float parameter from within
+                /// the base game we can change through this here.
+                animation.SetCurve("CardSprite", typeof(CardEffectTransform), "position.x", curve);
+                animation.wrapMode = WrapMode.Loop;
+            }
+
+            return (animation, sprites);
         }
 
         private void SetMaterialLayerProperties(Material material, int index, EffectLayer layer)
@@ -189,6 +358,7 @@ namespace TrainworksReloaded.Base.Prefab
                 stretch = layerConfig.GetSection("stretch").ParseBool() ?? true,
                 additive = layerConfig.GetSection("additive").ParseBool() ?? true,
                 enabled = layerConfig.GetSection("enabled").ParseBool() ?? true,
+                animated = layerConfig.GetSection("animated").ParseBool() ?? false,
             };
             return ret;
         }
@@ -231,7 +401,7 @@ namespace TrainworksReloaded.Base.Prefab
             };
         }
 
-        private void SetupStaticCardArt(IDefinition<GameObject> definition, IConfiguration config)
+        private void SetupSpriteBasedCardArt(IDefinition<GameObject> definition, IConfiguration config)
         {
             var spriteVal = config.GetSection("sprite").ParseReference();
             if (spriteVal == null)
@@ -265,7 +435,7 @@ namespace TrainworksReloaded.Base.Prefab
                 rectTransform.pivot = new Vector2(0.5f, 0.5f); // Center pivot
             }
 
-            var material = new Material(Shader.Find("Shiny Shoe/CardEffects"))
+            /*var material = new Material(Shader.Find("Shiny Shoe/CardEffects"))
             {
                 mainTexture = sprite.texture,
             };
@@ -275,7 +445,7 @@ namespace TrainworksReloaded.Base.Prefab
             canvasRenderer.SetMaterial(material, 0);
 
             var cardEffectTransforms = new GameObject { name = "CardEffectTransforms" };
-            cardEffectTransforms.transform.SetParent(gameObject.transform);
+            cardEffectTransforms.transform.SetParent(gameObject.transform);*/
         }
     }
 }
